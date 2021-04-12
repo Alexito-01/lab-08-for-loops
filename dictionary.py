2ea2b1d3a57d20dbb8896349a8f1a1e1c3f58c5f
Alexito-01 <78381199+Alexito-01@users.noreply.github.com>
2021-04-12 15:14:06 -0400

    Fixed the dictonary.py file
    I got a better understanding of doing dictionaries in python during class.

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,21 @@
+#myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+#for x, y in myData.items():
+#	print ("key: " + x, y)
+#print("ALL KEYS: ")
+#for x in myData:
+#	print (x)
+#print("ALL VALUES: ")
+#for x in myData.values():
+#	print (x)*/
 myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
-for x, y in myData.items():
-	print ("key: " + x, y)
-print("ALL KEYS: ")
-for x in myData:
-	print (x)
-print("ALL VALUES: ")
-for x in myData.values():
-	print (x)
+mykeys = []
+myvalues = []
+
+
+for k, v in myData.items():
+	print("key: " + str(k) + ", values: " + str(v))
+	mykeys.append(k)
+	myvalues.append(v)
+
+print("ALL KEYS: " + str(mykeys))
+print("ALL VALUES: " + str(myvalues))
